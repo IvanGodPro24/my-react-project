ac6711e58d18da443c8e87c2f311d59c47e3b27c
IvanGodPro24 <vanyanep@gmail.com>
2024-12-07 22:15:53 +0200

    Add example

---
+++ src/components/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import reactLogo from "../assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Hello from "./Hello";
+import Product from "./Product";
+import BookList from "./Book";
+
+const favouriteBooks = [
+  { id: "id-1", name: "JS for beginners" },
+  { id: "id-2", name: "React basics" },
+  { id: "id-3", name: "React Router overview" },
+];
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <Hello />
+      <Product
+        name="Tacos With Lime"
+        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+        price={10.99}
+      />
+      <Product
+        name="Fries and Burger"
+        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+        price={14.29}
+      />
+      <h1>Books of the week</h1>
+      <BookList books={favouriteBooks} />
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Book.jsx
@@ -0,0 +1,11 @@
+const BookList = ({ books }) => {
+  return (
+    <ul>
+      {books.map((book) => {
+        return <li>{book.name}</li>;
+      })}
+    </ul>
+  );
+};
+
+export default BookList

e0dbbfb60dac74eda93ceb1664234b7bc309bf07
IvanGodPro24 <vanyanep@gmail.com>
2025-01-18 10:25:17 +0200

    Add nested routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -28,6 +28,9 @@ import About from "../pages/About";
 import Products from "../pages/Products";
 import NotFound from "../pages/NotFound";
 import ProductDetails from "../pages/ProductDetails";
+import Mission from "./Mission";
+import Team from "./Team";
+import Reviews from "./Reviews";
 
 const buildLinkClass = ({ isActive }) => {
   return clsx(css.link, isActive && css.active);
@@ -256,7 +259,11 @@ function App() {
 
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/about" element={<About />}>
+            <Route path="mission" element={<Mission />} />
+            <Route path="team" element={<Team />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
           <Route path="/products" element={<Products />} />
           <Route path="/products/:productId" element={<ProductDetails />} />
           <Route path="*" element={<NotFound />} />

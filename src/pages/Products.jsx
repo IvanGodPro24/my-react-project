515e67aa42812c540753c144fed626051e9699c9
IvanGodPro24 <vanyanep@gmail.com>
2025-01-20 22:46:52 +0200

    Add filtration

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,12 +1,33 @@
+import { useSearchParams } from "react-router-dom";
 import { ProductList } from "../components/ProductList";
+import SearchBox from "../components/SearchBox";
 import { getProducts } from "../fakeApi";
 
 const Products = () => {
   const products = getProducts();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const productName = searchParams.get("name") ?? "";
+
+  const visibleProducts = products.filter((product) =>
+    product.name.toLowerCase().includes(productName.toLowerCase())
+  );
+
+  const updateSearchParams = (key, value) => {
+    const updateParams = new URLSearchParams();
+
+    if (value !== "") {
+      updateParams.set(key, value);
+    } else {
+      updateParams.delete(key)
+    }
+
+    setSearchParams(updateParams);
+  };
 
   return (
     <main>
-      <ProductList products={products} />
+      <SearchBox value={productName} onChange={(value) => updateSearchParams('name', value)} />
+      <ProductList products={visibleProducts} />
     </main>
   );
 };
